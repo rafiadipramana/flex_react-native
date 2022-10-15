c8dff3c509f7b4b368458d8e16c94c4cc9bb6151
rafiadipramana <rafiadipramana@gmail.com>
2022-10-16 02:02:46 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View
+        style = {{
+          flex: 1
+        }}
+      >
+
+        <View
+          style = {{
+            alignItems: 'center',
+            backgroundColor: 'lightgray',
+            flex: 1,
+            justifyContent: 'center'
+          }}
+        >
+          <Text>
+            Kosong masih belum ada item
+          </Text>
+        </View>
+
+        <View
+          style = {{
+            backgroundColor: 'grey',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            padding: 20
+          }}
+        >
+  
+          <View
+            style = {{
+              backgroundColor: 'yellow',
+              height: 30,
+              width: 30
+            }}
+          >
+          </View>
+
+          <View
+            style = {{
+              backgroundColor: 'white',
+              height: 30,
+              width: 30
+            }}
+          >
+          </View>
+
+          <View
+            style = {{
+              backgroundColor: 'white',
+              height: 30,
+              width: 30
+            }}
+          >
+          </View>
+        </View>
+      </View>
+    )
+  }
+}
